8e58f51dc5a82f8493f3975433f63af9dadae939
adnrussg <russelgarcia1551@gmail.com>
2024-10-30 08:59:15 -0400

    Inserted required imports

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,6 +1,10 @@
+const express = require('express');
+const router = express.Router();
 const connectToDatabase = require("../models/db");
+const logger = require('../logger');
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
+    logger.info('/called');
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
         const db = await connectToDatabase();
@@ -14,12 +18,12 @@ router.get('/', async (req, res) => {
         // Task 4: return the gifts using the res.json method
         res.json(gifts);
     } catch (e) {
-        console.error('Error fetching gifts:', e);
-        res.status(500).send('Error fetching gifts');
+        logger.console.error('oops something went wrong', e)
+        next(e);
     }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
         const db = await connectToDatabase();
@@ -38,8 +42,7 @@ router.get('/:id', async (req, res) => {
 
         res.json(gift);
     } catch (e) {
-        console.error('Error fetching gift:', e);
-        res.status(500).send('Error fetching gift');
+        next(e);
     }
 });
 
