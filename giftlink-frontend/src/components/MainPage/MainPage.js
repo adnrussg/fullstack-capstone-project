272973b03710f7447effb4706ddd54e74298ee5b
adnrussg <russelgarcia1551@gmail.com>
2024-11-02 21:09:22 -0400

    Displaying gift name and the date of it posted or listed

--- giftlink-frontend/src/components/MainPage/MainPage.js
+++ giftlink-frontend/src/components/MainPage/MainPage.js
@@ -63,14 +63,14 @@ function MainPage() {
 
                                 {/* // Task 5: Display gift image or placeholder */}
                                 {/* // Write your code below this line */}
-
+                                <h5 className='card-title'>{gift.name}</h5>
                                 <p className={`card-text ${getConditionClass(gift.condition)}`}>
                                 {gift.condition}
                                 </p>
 
                                 {/* // Task 6: Display gift image or placeholder */}
                                 {/* // Write your code below this line */}
-                                
+                                <p className='card-text'>{formatDate(gift.date_added)}</p>
 
                                 <button onClick={() => goToDetailsPage(gift.id)} className="btn btn-primary">
                                     View Details
