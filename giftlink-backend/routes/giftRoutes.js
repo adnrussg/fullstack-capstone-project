ad9edf29e607a6c1b22df9419eb78791874003d2
adnrussg <russelgarcia1551@gmail.com>
2024-11-04 01:28:37 -0500

    Changes

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res, next) => {
         // Task 4: return the gifts using the res.json method
         res.json(gifts);
     } catch (e) {
-        logger.console.error('oops something went wrong', e)
+        logger.error('oops something went wrong', e)
         next(e);
     }
 });
